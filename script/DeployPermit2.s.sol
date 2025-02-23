1f347c518ec97d9effa577424dae17a28ae067f8
cqlyj <luoyingjie0721@gmail.com>
2025-02-24 02:14:01 +0800

    feat: update DeployPermit2Bank script to use constant PERMIT2_ADDRESS and remove Vm dependency because the clone contract does not contain a contract name

--- script/DeployPermit2.s.sol
+++ script/DeployPermit2.s.sol
@@ -10,6 +10,9 @@ contract DeployPermit2 is Script {
         Permit2Clone permit2 = new Permit2Clone();
         vm.stopBroadcast();
         console.log("Deployed Permit2 at:", address(permit2));
+        console.log(
+            "Please update the Permit2 address in the DeployPermit2Bank script"
+        );
         return permit2;
     }
 }
--- script/DeployPermit2Bank.s.sol
+++ script/DeployPermit2Bank.s.sol
@@ -2,11 +2,14 @@
 pragma solidity 0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Vm} from "forge-std/Vm.sol";
 import {Permit2Bank} from "src/Permit2Bank.sol";
 
+// import {Vm} from "forge-std/Vm.sol";
+
 contract DeployPermit2Bank is Script {
-    address public permit2Clone;
+    // @Notice you need to manually set the permit2Clone address from the script output
+    address public constant PERMIT2_ADDRESS =
+        0x5FbDB2315678afecb367f032d93F642f64180aa3;
 
     function deployPermit2Bank(
         address _permit2Clone
@@ -21,12 +24,12 @@ contract DeployPermit2Bank is Script {
     }
 
     function run() external returns (Permit2Bank) {
-        permit2Clone = Vm(address(vm)).getDeployment(
-            "Permit2Clone",
-            uint64(block.chainid)
-        );
+        // permit2Clone = Vm(address(vm)).getDeployment(
+        //     "Permit2Clone",
+        //     uint64(block.chainid)
+        // );
 
-        Permit2Bank permit2Bank = deployPermit2Bank(permit2Clone);
+        Permit2Bank permit2Bank = deployPermit2Bank(PERMIT2_ADDRESS);
         return permit2Bank;
     }
 }
